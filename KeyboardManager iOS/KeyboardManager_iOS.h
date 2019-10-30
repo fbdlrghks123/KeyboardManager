9f8958d91db70ea2ce329c7234fff57125ce4c7b
rih <fbdlrghks123@naver.com>
2019-10-30 13:31:56 +0900

    0.2.0

---
+++ KeyboardManager iOS/KeyboardManager_iOS.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardManager_iOS.h
+//  KeyboardManager iOS
+//
+//  Created by ryuickhwan on 2019/10/30.
+//  Copyright © 2019 ryuickhwan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KeyboardManager_iOS.
+FOUNDATION_EXPORT double KeyboardManager_iOSVersionNumber;
+
+//! Project version string for KeyboardManager_iOS.
+FOUNDATION_EXPORT const unsigned char KeyboardManager_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardManager_iOS/PublicHeader.h>
+
+
